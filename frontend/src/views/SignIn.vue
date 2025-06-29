2f4fd3c3bb10ed6f280ef8791222f433ae22d15b
Justina <p930625q@gmail.com>
2025-06-29 11:40:10 +0800

    signIn final

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -2,7 +2,7 @@
     import Menu from '../components/Menu.vue'
     import { ref } from 'vue'
     import { useRouter } from 'vue-router'
-import router from '../router'
+    import router from '../router'
 
     const name = ref('')
     const sex = ref('')
@@ -13,13 +13,15 @@ import router from '../router'
     const grade = ref('')
     const interest = ref('')
     const skill = ref('')
+    const plan = ref('')
 
     function submit(){
-        if( !name.value || !email.value || !phone.value || !school.value || !major.value || !grade.value || !interest.value || !skill.value){
+        if( !name.value || !sex.value  || !email.value || !phone.value || !school.value || !major.value || !grade.value || !interest.value || !skill.value || !plan.value){
             alert('請完成所有表格')
             return
         }else{
             router.push({ name: 'Home'})
+            console.log(name.value,sex.value,email.value,phone.value,school.value,major,grade.value,interest.value,skill.value,plan.value)
         }
     }
 </script>
@@ -40,7 +42,14 @@ import router from '../router'
                 />
             </div>
             <div class="sexContainer">
-                <div class="sex">02. 性別：男 / 女</div>
+                <div class="sex">02. 性別：</div>
+                <div class="sp">/</div>
+                <label class="sexlabel male" :class="{ selected: sex === '男' }">   <!-- 點字就可以觸發input選取或輸入(連動的效果) -->
+                    <input type="radio" v-model="sex" value="男" class="hiddenRadio" />男
+                </label>  <!-- class的條件切換 如果sex的值等於特定內容時就多加一個class -->
+                <label class="sexlabel female" :class="{ selected: sex === '女' }">
+                    <input type="radio" v-model="sex" value="女" class="hiddenRadio" />女
+                </label>
             </div>
             <div class="emailContainer">
                 <div class="email">03. Email：</div>
@@ -107,6 +116,18 @@ import router from '../router'
             </div>
             <div class="planContainer">
                 <div class="plan">10. 目前對畢業後的規劃為？</div>
+                <label class="planlabel study" :class="{ checked: plan === '升學' }">
+                    <input type="radio" v-model="plan" value="升學" class="hiddenCheckbox"/>
+                    <span class="custom-checkbox"></span>升學
+                </label>
+                <label class="planlabel work" :class="{ checked: plan === '就業' }">
+                    <input type="radio" v-model="plan" value="就業" class="hiddenCheckbox"/>
+                    <span class="custom-checkbox"></span>就業
+                </label>
+                <label class="planlabel unknow" :class="{ checked: plan === '不清楚' }">
+                    <input type="radio" v-model="plan" value="不清楚" class="hiddenCheckbox"/>
+                    <span class="custom-checkbox"></span>不清楚
+                </label>
             </div>
             <button type="submit" class="submitButton"></button>
         </form>
@@ -114,7 +135,6 @@ import router from '../router'
 </template>
 
 <style scoped>
-/*|| !sex.value */
     .page-wrapper{
         background-color: white;
         min-height: 100vh;
@@ -148,9 +168,17 @@ import router from '../router'
         position: absolute;   
         top: 26%;             
         left: 50%;            
-        transform: translateX(-165%);
+        transform: translateX(-265%);
         font-size: 1.1rem;
         z-index: 10;
+    }
+    .sp{
+        position: absolute;   
+        top: 26.3%;             
+        left: 50%;            
+        transform: translateX(-1400%);
+        font-size: 1.3rem;
+        z-index: 10; 
     }
     .email{
         position: absolute;   
@@ -325,5 +353,78 @@ import router from '../router'
         background-color: transparent;
         outline: none;
         font-size: 18px;
+    }
+    .hiddenRadio {
+        display: none; /* 隱藏 radio 圈圈 */
+    }
+    .sexlabel {
+        position: absolute;
+        top: 25.7%;
+        font-size: 1.1rem;
+        cursor: pointer;
+        z-index: 11;
+        padding: 4px 10px;
+        border-radius: 100%;        /* 讓邊框變圓形 */
+        border: 5px solid transparent; /* 預設邊框透明 */
+        display: inline-flex;      /* 讓文字跟邊框合在一起 */
+        transition: background-color 0.3s;
+        align-items: center;
+        justify-content: center;
+        width: 5px;               /* 你想要的圈大小 */
+        height: 15px;
+    }
+    .sexlabel.male {
+        left: 50%;  /* 你想放哪裡自己調整 */
+        transform: translateX(-355%);
+    }
+    .sexlabel.female {
+        left: 50%;  /* 右邊一點 */
+        transform: translateX(-175%);
+    }
+    .sexlabel:hover {
+        border-color: #e24f4f;
+    }
+    .sexlabel.selected {
+        border-color: #b73131; /* 被選中時的背景色 */
+    }
+    .hiddenCheckbox{
+        display: none;
+    }
+    .custom-checkbox {
+        display: inline-block;
+        width: 45px;
+        height: 45px;
+        background-image: url('../assets/images/signin/checkbox.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        flex-shrink: 0;
+}
+    .planlabel{
+        position: absolute;
+        left: 50%;
+        font-size: 1.1rem;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        z-index: 13;
+    }
+    .planlabel.study{
+        top: 62.5%;
+        transform: translateX(-180%);
+    }
+    .planlabel.work{
+        top: 67%;
+        transform: translateX(-180%);
+    }
+    .planlabel.unknow{
+        top: 71.5%;
+        transform: translateX(-151%);
+    }
+    .planlabel.checked .custom-checkbox {
+        background-image: url('../assets/images/signin/checked.png');
+        background-size: 38px 35px;
+        transform: translateX(-10%);
+        transform: translateY(-12.5%);
     }
 </style>
